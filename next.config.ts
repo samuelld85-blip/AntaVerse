d9bb6f2a09f5c0ab9dd842448a14b562c2820e25
SLChaumet <s.ledu-ext@chaumet.com>
2026-08-20 16:12:53 +0200

    FEAT_LEGAL

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  images: { unoptimized: true },
+  images: { unoptimized: true, qualities: [75, 95] },
   reactStrictMode: true,
   poweredByHeader: false,
   typedRoutes: true,
